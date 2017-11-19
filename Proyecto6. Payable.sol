6728ab7177016fbc7eb1727c7636bbadaba83a16
Jorge disRupt <32745694+disruptavila@users.noreply.github.com>
2017-11-19 19:42:27 +0800

    Create Proyecto6. Payable.sol

---
+++ Proyecto6. Payable.sol
@@ -0,0 +1,11 @@
+/*	Comentarios sobre lo que el contracto realiza */
+/*
+ * 	Nombre: proyecto6.sol
+ *	Version:	v0.1
+ *	Autor:	Jorge Martin
+ *	Fecha:	noviembre 2017
+ *	Descripcion:		Payable. - Payable.sol
+ *
+ */
+
+/* Declaracion pragma */
